d54964172db95afa6061207fc179f9fdef97c5b8
Trevor Nagy <tnagy@nccinc.ca>
2018-12-02 22:16:05 -0500

    Added files for the pics project.

---
+++ src/api/unsplash.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+import config from "../config";
+
+export default axios.create({
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: "Client-ID " + config.unsplashKey
+  }
+});
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  unsplashKey:
+    "4620a86834f84397bd4686df707e4f434b49c149d76a6f7db77123906e107ffd"
+};
